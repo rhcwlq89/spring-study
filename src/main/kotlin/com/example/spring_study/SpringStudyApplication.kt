c5f1516e862b373c819ab7e6c2a488d7cb2f4879
이호종 <ccaja21@naver.com>
2024-12-13 16:05:32 +0900

    init

---
+++ src/main/kotlin/com/example/spring_study/SpringStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringStudyApplication>(*args)
+}
